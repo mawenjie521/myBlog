2fce2bc4fb95f1853aae20989101ec6bc4a034cb
mawenjie <mawenjie@jiuqi.com.cn>
2020-07-02 10:49:35 +0800

    new file

---
+++ project/static/output/vue/precache-manifest.9787f298c692b9372cd12e3542994da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7343225ae0de8ec41893",
+    "url": "/app.bundle.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2ce2bd51e55343d1113776a8b7cbf5be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a977512a6cd9c19a51b5",
+    "url": "/js/about.d7bcb877.js"
+  },
+  {
+    "revision": "29b9b23d8617856301e90d8d8f2885b2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ web/vue.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+let BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const root = path.resolve(__dirname, '..'); // 根目录
+
+console.log(root)
+var config = {
+  outputDir: path.resolve(root, 'project/static/output/vue'),
+  configureWebpack: {
+    devtool: 'eval-source-map',
+    output: {
+        library: 'portal_application',
+        libraryTarget: 'window',
+        libraryExport: 'default',
+        filename: 'app.bundle.js'
+    }
+  },
+  chainWebpack: config => {
+    config.optimization.delete('splitChunks')
+  },
+  css: { extract: false },
+  lintOnSave: false,
+  productionSourceMap: false,
+}
+
+module.exports = config
