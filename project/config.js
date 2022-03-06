84662a36ea14c01a8ecd24a2b33b04c54b58896b
mawenjie521 <mawenjie14@jd.com>
2022-03-06 11:26:04 +0800

    修改config

--- project/config.js
+++ project/config.js
@@ -1,11 +1,14 @@
 const config = {
 
   port: 3001,
-
+  /*
+    sql user;
+    sudo cat /etc/mysql/debian.cnf
+  */
   database: {
     DATABASE: 'koa_demo',
-    USERNAME: 'debian-sys-maint',
-    PASSWORD: 'ejg5mBHjQ4UnfGZl',
+    USERNAME: 'root',
+    PASSWORD: '123456',
     HOST: 'localhost'
   }
 }
